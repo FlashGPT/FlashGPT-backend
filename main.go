73173a0f87716327f3a7deb5bbd2f32da7c022d4
kurt <kurtyjlee@gmail.com>
2024-02-17 02:42:46 +0800

    Create test requests

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-    app.Listen(":3000")
+	app.Post("/", func(c *fiber.Ctx) error {
+		type BodyModel struct {
+			Message string `json:"message" xml:"message" form:"message"`
+		}
+
+		body := new(BodyModel)
+
+		if err := c.BodyParser(body); err != nil {
+			return err
+		}
+
+		return c.JSON(fiber.Map{
+			"message": body.Message,
+			"status":  "success",
+		})
+	})
+
+	app.Listen(":8080")
 }
